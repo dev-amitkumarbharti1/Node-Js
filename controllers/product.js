8700c62a82077b005c25f36e7db18b7aad501787
Your Name <akbharti1717@gmail.com>
2022-07-05 02:05:57 +0530

    new update

--- controllers/product.js
+++ controllers/product.js
@@ -2,3 +2,6 @@
 exports.getIndex=((req,res,next)=>{
     res.render("product/index");
 })
+exports.addProduct=((req,res,next)=>{
+    res.render("product/add-product");
+})
--- routing/productRoute.js
+++ routing/productRoute.js
@@ -5,5 +5,6 @@ const productController=require("../controllers/product");
 const router=express.Router();
 
 router.get('/get-product',productController.getIndex);
+router.get('/add-product',productController.addProduct);
 
 module.exports=router;
